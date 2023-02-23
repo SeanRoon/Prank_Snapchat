519f892ccf62fe4eb33bc88a028d5f26b9977cfa
rooneys24 <rooneysw@gmail.com>
2023-02-23 11:57:15 -0500

    3

---
+++ app/src/main/java/com/example/pranksnapchat/SnapViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.pranksnapchat
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pranksnapchat.databinding.ListItemLayoutBinding
+
+class SnapViewHolder (val binding: ListItemLayoutBinding):
+    RecyclerView.ViewHolder(binding.root) {
+
+}
